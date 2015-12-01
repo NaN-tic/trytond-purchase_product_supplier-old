485227914bfa2caa5494dc5fe30ec4605d9e86b1
resteve <resteve@zikzakmedia.com>
2015-12-01 11:17:35 +0100

    Tests

--- tests/test_purchase_product_supplier.py
+++ tests/test_purchase_product_supplier.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part purchase_product_supplier module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the purchase_product_supplier module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class PurchaseProductSupplierTestCase(unittest.TestCase):
+class PurchaseProductSupplierTestCase(ModuleTestCase):
     'Test Purchase Product Supplier module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('purchase_product_supplier')
-
-    def test0005views(self):
-        'Test views'
-        test_view('purchase_product_supplier')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'purchase_product_supplier'
 
 
 def suite():
@@ -27,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         PurchaseProductSupplierTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
